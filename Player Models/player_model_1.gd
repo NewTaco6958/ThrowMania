76e55a98e832082f232fbd91ed182c304fda2079
NewTaco6958 <bh405023@gmail.com>
2025-07-17 18:50:58 -0400

    Worked on the shooting mechanism it sucks ngl

--- Player Models/player_model_1.gd
+++ Player Models/player_model_1.gd
@@ -3,12 +3,26 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
+	
+@onready var main = get_node("spawn")
+@onready var projectile = load("res://Projectiles/rock.tscn")
 
+
+func shoot():
+	var instance = projectile.instantiate()
+	instance.dir = rotation
+	instance.spawnPos = global_position
+	instance.spawnRot = rotation
+	main.add_child.call_deferred(instance)
+	
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+		
+	if Input.is_action_just_pressed("Throw"):
+		shoot()
 		
 	
 	
