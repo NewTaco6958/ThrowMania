916c1a4539ce2e85c77970bac1f3e3d29434b60f
Sap-has <sarinanaepifanio@gmail.com>
2025-07-16 18:54:39 -0600

    Fixed Error

--- Player Models/player_model_1.gd
+++ Player Models/player_model_1.gd
@@ -20,18 +20,10 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-<<<<<<< HEAD
 		
 	if Input.is_action_just_pressed("Throw"):
 		shoot()
-		
-	
-	
-=======
 
-
-
->>>>>>> 5419cb207f886d0e91f94991d2fe8d996f1b3b3d
 	move_and_slide()
 	
 	
